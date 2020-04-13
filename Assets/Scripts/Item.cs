51d586712a5cf2969b993953604cff72fac03404
Charles Averill <charlesaverill20@gmail.com>
2020-04-13 12:46:54 -0500

    Fixed scaling issues when objects knocked down

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool beginning_scale_fix;
+    public Transform cam;
+    public Transform initial_parent;
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        transform.parent = cam;
+        beginning_scale_fix = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Debug.Log(transform.parent);
+      if(transform.parent == cam && beginning_scale_fix){
+        GetComponent<Rigidbody>().useGravity = true;
+        transform.parent = null;
+        beginning_scale_fix = false;
+      }
+    }
+}
